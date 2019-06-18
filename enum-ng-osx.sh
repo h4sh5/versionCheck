edd5d91f14beba8f11b1bdd1b9fe88285b5d8333
Haoxi Tan <38898566+h4sh5@users.noreply.github.com>
2019-06-18 12:47:22 +1000

    added scripts for windows and osx

---
+++ enum-ng-osx.sh
@@ -0,0 +1,9 @@
+#system
+for p in /Applications/*
+do echo $p, `plutil -p "$p/Contents/Info.plist"  | grep --colour=no -i CFBundleShortVersionString | cut -d '>' -f 2 2>&1`, `plutil -p "$p/Contents/Info.plist"  | grep --colour=no -i CFBundleVersion | cut -d '>' -f 2 2>&1`  
+done
+
+#user
+for p in ~/Applications/*
+do echo $p, `plutil -p "$p/Contents/Info.plist"  | grep --colour=no -i CFBundleShortVersionString | cut -d '>' -f 2 2>&1`, `plutil -p "$p/Contents/Info.plist"  | grep --colour=no -i CFBundleVersion | cut -d '>' -f 2 2>&1` 
+done
