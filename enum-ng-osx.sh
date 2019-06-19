f1a7195bbe338c13ac0941f5bffbe76ab5955221
Haoxi Tan <38898566+h4sh5@users.noreply.github.com>
2019-06-19 10:40:10 +1000

    added flash version detection (yah, lame, i know)

--- enum-ng-osx.sh
+++ enum-ng-osx.sh
@@ -7,3 +7,8 @@ done
 for p in ~/Applications/*
 do echo $p, `plutil -p "$p/Contents/Info.plist"  | grep --colour=no -i CFBundleShortVersionString | cut -d '>' -f 2 2>&1`, `plutil -p "$p/Contents/Info.plist"  | grep --colour=no -i CFBundleVersion | cut -d '>' -f 2 2>&1` 
 done
+
+#flash
+for p in /Library/Internet\ Plug-Ins/*
+do echo $p, `plutil -p "$p/Contents/Info.plist"  | grep --colour=no -i CFBundleShortVersionString | cut -d '>' -f 2 2>&1`, `plutil -p "$p/Contents/Info.plist"  | grep --colour=no -i CFBundleVersion | cut -d '>' -f 2 2>&1`
+done
